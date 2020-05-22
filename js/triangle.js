c23cb6f8e461e07f1ad9b8609e7531f1e0766d1c
marcomoauro <marco@wanderio.com>
2020-05-22 12:42:19 +0200

    continue implementation of logic

--- js/triangle.js
+++ js/triangle.js
@@ -1,28 +1,64 @@
 let margin = { top: 20, right: 20, bottom: 30, left: 40 }
 let width = 1920 - margin.left - margin.right
 let height = 1080 - margin.top - margin.bottom
+let stroke = 'black'
+let strokeWidth = '2'
+
+let createSvg = () =>
+  d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+
+let drawTriangles = (svg, data) => data.forEach(element => drawTriangle(svg, element))
 
 let drawTriangle = (svg, element) => {
+  let height = triangleHeight(element.sides_length, element.base_length)
+  let topX = element.horizontal_position, topY = element.vertical_position
+  let downRightX = topX + element.base_length / 2, downRightY = topY + height
+  let downLeftX = downRightX - element.base_length, downLeftY = topY + height
+
+  // path use to fill color
   svg.append('path')
     .attr('d', d => {
-      let x = element.horizontal_position, y = element.vertical_position
-      let h = Math.sqrt(Math.pow(element.sides_length, 2) - Math.pow(element.base_length / 2, 2))
-      return 'M ' + x + ' ' + y + ' l ' + element.base_length / 2 + ' ' + h + ' h ' + -element.base_length + ' z'
+      return 'M ' + topX + ' ' + topY + ' L ' + downRightX + ' ' + downRightY + ' L ' + downLeftX + ' ' + downLeftY + ' L ' + topX + ' ' + topY
     })
+    .attr('fill', 'red')
+
+  // path lines
+  svg.append('path')
+    .attr('d', d => {
+      return 'M ' + topX + ' ' + topY + ' L ' + downRightX + ' ' + downRightY
+    })
+    .attr('stroke', stroke)
+    .attr('stroke-width', strokeWidth)
+
+  svg.append('path')
+    .attr('d', d => {
+      return 'M ' + downRightX + ' ' + downRightY + ' L ' + downLeftX + ' ' + downLeftY
+    })
+    .attr('stroke', stroke)
+    .attr('stroke-width', strokeWidth)
+
+  svg.append('path')
+    .attr('d', d => {
+      return 'M ' + downLeftX + ' ' + downLeftY + ' L ' + topX + ' ' + topY
+    })
+    .attr('stroke', stroke)
+    .attr('stroke-width', strokeWidth)
 }
+
+let triangleHeight = (sides_length, base_length) =>
+  Math.sqrt(Math.pow(sides_length, 2) - Math.pow(base_length / 2, 2))
 
 d3.json("data/triangles.json")
   .then(data => {
 
-    let svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-
-    data.forEach(element => drawTriangle(svg, element))
+    let svg = createSvg()
+    drawTriangles(svg, data)
 
 
 
   })
   .catch(error => {
     console.log(error)
-  });
+  })
