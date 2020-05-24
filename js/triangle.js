acfc900ebb9b238f6ef956f90099063b914b38e8
marcomoauro <marco@wanderio.com>
2020-05-24 16:08:05 +0200

    enter update exit pattern

--- js/triangle.js
+++ js/triangle.js
@@ -1,8 +1,9 @@
 let margin = { top: 20, right: 20, bottom: 30, left: 40 }
-let width = 1920 - margin.left - margin.right
-let height = 1080 - margin.top - margin.bottom
+let width = 1000 - margin.left - margin.right
+let height = 1000 - margin.top - margin.bottom
 let lineStroke = 'black'
 let lineWidth = '5'
+let animationDuration = 1000
 
 d3.json("data/triangles.json")
   .then(dataset => {
@@ -12,19 +13,22 @@ d3.json("data/triangles.json")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
 
-    let drawTriangles = (svg, dataset) => {
-      let groups = svg.selectAll('g').data(dataset)
+    let drawTriangles = (dataset) => {
+      console.log(dataset)
+      let triangleGroups = svg.selectAll('.triangle').data(dataset)
 
-      groups.exit().remove()
+      // enter
 
-      let group = groups.enter()
+      let triangleGroupsEnter = triangleGroups.enter()
         .append('g')
+        .attr('class', 'triangle')
 
-      group.append('path')
+      triangleGroupsEnter.append('path')
         .attr('d', element => trianglePath(element))
         .attr('fill', element => element.hue)
 
-      group.append('line')
+      triangleGroupsEnter.append('line')
+        .attr('class', 'right_side')
         .attr('x1', element => topX(element))
         .attr('y1', element => topY(element))
         .attr('x2', element => downRightX(element))
@@ -32,9 +36,10 @@ d3.json("data/triangles.json")
         .attr('stroke', lineStroke)
         .attr('stroke-width', lineWidth)
         .attr('stroke-linecap', "round")
-        .on('click', click)
+        .on('click', sideClick)
 
-      group.append('line')
+      triangleGroupsEnter.append('line')
+        .attr('class', 'base')
         .attr('x1', element => downRightX(element))
         .attr('y1', element => downRightY(element))
         .attr('x2', element => downLeftX(element))
@@ -42,9 +47,10 @@ d3.json("data/triangles.json")
         .attr('stroke', lineStroke)
         .attr('stroke-width', lineWidth)
         .attr('stroke-linecap', "round")
-        .on('click', click)
+        .on('click', baseClick)
 
-      group.append('line')
+      triangleGroupsEnter.append('line')
+        .attr('class', 'left_side')
         .attr('x1', element => downLeftX(element))
         .attr('y1', element => downLeftY(element))
         .attr('x2', element => topX(element))
@@ -52,20 +58,68 @@ d3.json("data/triangles.json")
         .attr('stroke', lineStroke)
         .attr('stroke-width', lineWidth)
         .attr('stroke-linecap', "round")
-        .on('click', click)
+        .on('click', sideClick)
+
+      // update
+
+      triangleGroups
+        .select('path')
+        .transition().duration(1000)
+        .attr('d', element => trianglePath(element))
+
+      triangleGroups
+        .select('.right_side')
+        .transition().duration(animationDuration)
+        .attr('x1', element => topX(element))
+        .attr('y1', element => topY(element))
+        .attr('x2', element => downRightX(element))
+        .attr('y2', element => downRightY(element))
+
+      triangleGroups
+        .select('.base')
+        .transition().duration(animationDuration)
+        .attr('x1', element => downRightX(element))
+        .attr('y1', element => downRightY(element))
+        .attr('x2', element => downLeftX(element))
+        .attr('y2', element => downLeftY(element))
+
+      triangleGroups
+        .select('.left_side')
+        .transition().duration(animationDuration)
+        .attr('x1', element => downLeftX(element))
+        .attr('y1', element => downLeftY(element))
+        .attr('x2', element => topX(element))
+        .attr('y2', element => topY(element))
+
+      // exit
+
+      triangleGroups.exit().remove()
     }
 
-    let click = d => {
-      let newDataset = dataset.map(element => {
+    let baseClick = d => {
+      dataset = dataset.map(element => {
         return {
-          'horizontal_position': element.base_length,
-          "vertical_position": element.vertical_position,
-          "base_length": d.horizontal_position,
+          'horizontal_position': element.horizontal_position,
+          "vertical_position": element.base_length,
+          "base_length": element.vertical_position,
           "sides_length": element.sides_length,
           "hue": element.hue
         }
       })
-      drawTriangles(svg, newDataset)
+      drawTriangles(dataset)
+    }
+
+    let sideClick = d => {
+      dataset = dataset.map(element => {
+        return {
+          'horizontal_position': element.horizontal_position,
+          "vertical_position": element.sides_length,
+          "base_length": element.base_length,
+          "sides_length": element.vertical_position,
+          "hue": element.hue
+        }
+      })
+      drawTriangles(dataset)
     }
 
     let trianglePath = element => {
@@ -86,7 +140,8 @@ d3.json("data/triangles.json")
     let triangleHeight = (sides_length, base_length) => Math.sqrt(Math.pow(sides_length, 2) - Math.pow(base_length / 2, 2))
 
     let svg = createSvg()
-    drawTriangles(svg, dataset)
+    drawTriangles(dataset)
+
 
   })
   .catch(error => {
