2b9e398c35688c51c6a0a90ebd454936b1bd8bfa
marcomoauro <marco@wanderio.com>
2020-05-25 16:15:02 +0200

    style

--- js/triangle.js
+++ js/triangle.js
@@ -1,12 +1,12 @@
 let animationDuration = 1000
 
-d3.json("data/triangles.json")
+d3.json('data/triangles.json')
   .then(dataset => {
 
     let createSvg = () =>
-      d3.select("body").append("svg")
-        .attr("width", 1200)
-        .attr("height", 1500)
+      d3.select('body').append('svg')
+        .attr('width', 1200)
+        .attr('height', 1500)
 
     let drawTriangles = (dataset) => {
       let triangleGroups = svg.selectAll('.triangle').data(dataset)
@@ -19,9 +19,9 @@ d3.json("data/triangles.json")
 
       triangleGroupsEnter.append('path')
         .attr('d', element => trianglePath(element))
-        .attr('fill', element => `hsl(${element.hue}, 100%, 50%)`)
+        .attr('fill', element => pathColor(element))
         .on('click', pathClickLeft)
-        .on("contextmenu", pathClickRight)
+        .on('contextmenu', pathClickRight)
 
       triangleGroupsEnter.append('line')
         .attr('class', 'right_side')
@@ -30,7 +30,7 @@ d3.json("data/triangles.json")
         .attr('x2', element => downRightX(element))
         .attr('y2', element => downRightY(element))
         .on('click', sideClickLeft)
-        .on("contextmenu", sideClickRight)
+        .on('contextmenu', sideClickRight)
 
       triangleGroupsEnter.append('line')
         .attr('class', 'base')
@@ -39,7 +39,7 @@ d3.json("data/triangles.json")
         .attr('x2', element => downLeftX(element))
         .attr('y2', element => downLeftY(element))
         .on('click', baseClickLeft)
-        .on("contextmenu", baseClickRight)
+        .on('contextmenu', baseClickRight)
 
       triangleGroupsEnter.append('line')
         .attr('class', 'left_side')
@@ -48,7 +48,7 @@ d3.json("data/triangles.json")
         .attr('x2', element => topX(element))
         .attr('y2', element => topY(element))
         .on('click', sideClickLeft)
-        .on("contextmenu", sideClickRight)
+        .on('contextmenu', sideClickRight)
 
       // update
 
@@ -56,7 +56,7 @@ d3.json("data/triangles.json")
         .select('path')
         .transition().duration(animationDuration)
         .attr('d', element => trianglePath(element))
-        .attr('fill', element => `hsl(${element.hue}, 100%, 50%)`)
+        .attr('fill', element => pathColor(element))
 
       triangleGroups
         .select('.right_side')
@@ -87,82 +87,84 @@ d3.json("data/triangles.json")
       triangleGroups.exit().remove()
     }
 
-    let pathClickLeft = d => {
+    let pathColor = element => `hsl(${element.hue}, 100%, 50%)`
+
+    let pathClickLeft = () => {
       dataset = dataset.map(element => {
         return {
           'horizontal_position': element.horizontal_position,
-          "vertical_position": element.hue,
-          "base_length": element.base_length,
-          "sides_length": element.sides_length,
-          "hue": element.vertical_position
+          'vertical_position': element.hue,
+          'base_length': element.base_length,
+          'sides_length': element.sides_length,
+          'hue': element.vertical_position
         }
       })
       drawTriangles(dataset)
     }
 
-    let pathClickRight = d => {
+    let pathClickRight = () => {
       d3.event.preventDefault()
       dataset = dataset.map(element => {
         return {
           'horizontal_position': element.hue,
-          "vertical_position": element.vertical_position,
-          "base_length": element.base_length,
-          "sides_length": element.sides_length,
-          "hue": element.horizontal_position
+          'vertical_position': element.vertical_position,
+          'base_length': element.base_length,
+          'sides_length': element.sides_length,
+          'hue': element.horizontal_position
         }
       })
       drawTriangles(dataset)
     }
 
-    let baseClickLeft = d => {
+    let baseClickLeft = () => {
       dataset = dataset.map(element => {
         return {
           'horizontal_position': element.horizontal_position,
-          "vertical_position": element.base_length,
-          "base_length": element.vertical_position,
-          "sides_length": element.sides_length,
-          "hue": element.hue
+          'vertical_position': element.base_length,
+          'base_length': element.vertical_position,
+          'sides_length': element.sides_length,
+          'hue': element.hue
         }
       })
       drawTriangles(dataset)
     }
 
-    let sideClickLeft = d => {
+    let sideClickLeft = () => {
       dataset = dataset.map(element => {
         return {
           'horizontal_position': element.horizontal_position,
-          "vertical_position": element.sides_length,
-          "base_length": element.base_length,
-          "sides_length": element.vertical_position,
-          "hue": element.hue
+          'vertical_position': element.sides_length,
+          'base_length': element.base_length,
+          'sides_length': element.vertical_position,
+          'hue': element.hue
         }
       })
       drawTriangles(dataset)
     }
 
-    let baseClickRight = d => {
+    let baseClickRight = () => {
       d3.event.preventDefault()
       dataset = dataset.map(element => {
         return {
           'horizontal_position': element.base_length,
-          "vertical_position": element.vertical_position,
-          "base_length": element.horizontal_position,
-          "sides_length": element.sides_length,
-          "hue": element.hue
+          'vertical_position': element.vertical_position,
+          'base_length': element.horizontal_position,
+          'sides_length': element.sides_length,
+          'hue': element.hue
         }
       })
       drawTriangles(dataset)
     }
 
-    let sideClickRight = d => {
+    let sideClickRight = () => {
       d3.event.preventDefault()
       dataset = dataset.map(element => {
         return {
           'horizontal_position': element.sides_length,
-          "vertical_position": element.vertical_position,
-          "base_length": element.base_length,
-          "sides_length": element.horizontal_position,
-          "hue": element.hue
+          'vertical_position': element.vertical_position,
+          'base_length': element.base_length,
+          'sides_length': element.horizontal_position,
+          'hue': element.hue
         }
       })
       drawTriangles(dataset)
